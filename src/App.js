bf61dd22232b7f4db2c7651bce3bf21f7fb763ff
Dev Mentis <reactdev.mentis@yandex.com>
2019-05-27 17:11:44 +0530

    changing from public to root

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
+import { FacebookProvider, Like, SendToMessenger } from 'react-facebook';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <p>
-            The Site is up and running.
+          The Site is up and running.
         </p>
-    
+        <FacebookProvider appId="2194136580678056">
+          <Like href="http://www.facebook.com" colorScheme="dark" />
+          <SendToMessenger messengerAppId="123456789" pageId="123456789" />
+        </FacebookProvider>
+         
       </header>
     </div>
   );
